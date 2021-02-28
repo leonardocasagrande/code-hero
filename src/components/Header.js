316f5823f49901c6c33b82bf7d1cb7bf5cb08d44
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-02-28 20:17:07 -0300

    Adicionado header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import '../styles/components/Header.css';
+
+export function Header() {
+    return (
+        <header className='header-container'>
+            <img src="/logo.svg" alt="Objective" />
+            <div>
+                <div>
+                    <span className='name-container'>Leonardo Casagrande</span>
+                    <span>Teste de Front End</span>
+                </div>
+                <span className='picture-container'>LC</span>
+            </div>
+        </header>
+    );
+}
